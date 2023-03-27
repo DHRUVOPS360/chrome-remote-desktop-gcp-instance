a348ba828ce2278216c259641810d4400614abf6
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-27 12:45:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "google_compute_instance" "chrome_desktop" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
 
