0e3ba53165b6b9b653e026df5190ac5ef74ac1bc
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-27 21:15:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,22 +17,79 @@ resource "google_compute_instance" "chrome_desktop" {
   }
 
   metadata_startup_script = <<-EOF
-    # Install Chrome Remote Desktop
-    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-    sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+#!/bin/bash -x
+#
+# Startup script to install Chrome remote desktop and a desktop environment.
+#
+# See environmental variables at then end of the script for configuration
+#
 
+function install_desktop_env {
+  PACKAGES="desktop-base xscreensaver dbus-x11"
 
-    # Configure Gnome System desktop environment
-    sudo DEBIAN_FRONTEND=noninteractive \
-    apt install --assume-yes  task-gnome-desktop
-    sudo bash -c ‘echo “exec /etc/X11/Xsession /usr/bin/gnome-session” > /etc/chrome-remote-desktop-session’
-    sudo systemctl disable gdm3.service
-    sudo reboot
-    curl -L -o google-chrome-stable_current_amd64.deb \
-    https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb
-    EOF
-    tags = ["chrome-desktop"]
+  if [[ "$INSTALL_XFCE" != "yes" && "$INSTALL_CINNAMON" != "yes" ]] ; then
+    # neither XFCE nor cinnamon specified; install both
+    INSTALL_XFCE=yes
+    INSTALL_CINNAMON=yes
+  fi
+
+  if [[ "$INSTALL_XFCE" = "yes" ]] ; then
+    PACKAGES="$PACKAGES xfce4"
+    echo "exec xfce4-session" > /etc/chrome-remote-desktop-session
+    [[ "$INSTALL_FULL_DESKTOP" = "yes" ]] && \
+      PACKAGES="$PACKAGES task-xfce-desktop"
+  fi
+
+  if [[ "$INSTALL_CINNAMON" = "yes" ]] ; then
+    PACKAGES="$PACKAGES cinnamon-core"
+    echo "exec cinnamon-session-cinnamon2d" > /etc/chrome-remote-desktop-session
+    [[ "$INSTALL_FULL_DESKTOP" = "yes" ]] && \
+      PACKAGES="$PACKAGES task-cinnamon-desktop"
+  fi
+
+  DEBIAN_FRONTEND=noninteractive \
+    apt-get install --assume-yes $PACKAGES $EXTRA_PACKAGES
+
+  systemctl disable lightdm.service
+}
+
+function download_and_install { # args URL FILENAME
+  curl -L -o "$2" "$1"
+  apt-get install --assume-yes --fix-broken "$2"
+}
+
+function is_installed {  # args PACKAGE_NAME
+  dpkg-query --list "$1" | grep -q "^ii" 2>/dev/null
+  return $?
+}
+
+# Configure the following environmental variables as required:
+INSTALL_XFCE=yes
+INSTALL_CINNAMON=yes
+INSTALL_CHROME=yes
+INSTALL_FULL_DESKTOP=yes
+
+# Any additional packages that should be installed on startup can be added here
+EXTRA_PACKAGES="less bzip2 zip unzip tasksel wget"
+
+apt-get update
+
+! is_installed chrome-remote-desktop && \
+  download_and_install \
+    https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb \
+    /tmp/chrome-remote-desktop_current_amd64.deb
+
+install_desktop_env
+
+[[ "$INSTALL_CHROME" = "yes" ]] && \
+  ! is_installed google-chrome-stable && \
+  download_and_install \
+    https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
+    /tmp/google-chrome-stable_current_amd64.deb
+
+echo "Chrome remote desktop installation completed"
+  EOF
+  tags                    = ["chrome-desktop"]
 }
 
 resource "google_compute_firewall" "chrome_desktop" {
