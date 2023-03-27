da04990d651c600788aeec84731f1b34c5610aed
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-27 13:43:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,16 +10,19 @@ resource "google_compute_instance" "chrome_desktop" {
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
-    sudo apt-get update
-    sudo apt-get install -y xfce4 desktop-base xscreensaver
-    sudo apt-get install -y chrome-remote-desktop
-    sudo apt-get install -y task-xfce-desktop
-    echo "xfce4-session" > ~/.chrome-remote-desktop-session
-    sudo usermod -a -G chrome-remote-desktop ${USER}
-    sudo adduser ${USER} chrome-remote-desktop
+    sudo apt update
+    curl -L -o chrome-remote-desktop_current_amd64.deb \
+        https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+    sudo DEBIAN_FRONTEND=noninteractive \
+        apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+    sudo DEBIAN_FRONTEND=noninteractive \
+        apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
     sudo systemctl disable lightdm.service
-    sudo systemctl enable chrome-remote-desktop.service
-    sudo systemctl start chrome-remote-desktop.service
+    sudo apt install --assume-yes task-xfce-desktop
+    curl -L -o google-chrome-stable_current_amd64.deb \
+        https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb
   EOF
 
   tags = ["chrome-desktop"]
