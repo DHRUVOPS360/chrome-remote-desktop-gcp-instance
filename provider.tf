563538aa5f2935da59d843a412a21081a435e291
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-27 14:06:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.57.0"
+    }
+  }
+}
+
 provider "google" {
-  credentials = var.GOOGLE_APPLICATION_CREDENTIALS
   project = "engineer-cloud-nprod"
   region  = "us-central1"
+  zone    = "us-central1-c"
 }
