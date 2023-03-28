21248ced7bffa142f8b88c7ab11ed44560e183a8
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-28 20:16:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*resource "google_compute_instance" "chrome_desktop" {
+resource "google_compute_instance" "chrome_desktop" {
   name         = "chrome-remote-desktop"
   machine_type = "e2-medium"
 
@@ -29,4 +29,3 @@ resource "google_compute_firewall" "chrome_desktop" {
   }
   source_ranges = ["0.0.0.0/0"]
 }
-*/
